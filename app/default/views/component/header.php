e817e979aa95f57035bdcc00ecd61d0da0b792ce
kimduc0103 <hoangkimanhduc01031999@gmail.com>
2021-11-27 03:43:09 +0700

    anh duc rating cho feedback lan 2 sua file header trong default view lai nhu cu

--- app/default/views/component/header.php
+++ app/default/views/component/header.php
@@ -13,8 +13,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="./public/default/css/style.css">
-    <link href="/public/default/duc/bootstrap-rating.css" rel="stylesheet">
-<script type="text/javascript" src="/public/default/duc/bootstrap-rating.js"></script>
+
 </head>
 <body>
 
