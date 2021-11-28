3179d5f070d95c915e8de1e401c97c805b8f8689
nguyendao3105 <56407162+nguyendao3105@users.noreply.github.com>
2021-11-28 23:03:14 +0700

    adjust news ui

--- app/default/views/news/index.php
+++ app/default/views/news/index.php
@@ -14,13 +14,13 @@
 ?>
 <section class="vh-100 d-flex justify-content-center" id="content">
     <div style="width:70vw">
-        <div class="row">
+        <div class="row mt-2">
             <div class="col-6 px-0" style="background-color:white">
-                <img  style="min-height:50vh;width:100%;border-radius:0;" src="public/images/image_news_1.jpg" alt="Description to news">
+                <img  style="max-height:60vh;width:100%;border-radius:0;box-shadow: rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;" src="public/images/news1_bg.jpg" alt="Description to news">
                 <h2 style="position: absolute;top: 8px;left: 16px;cursor:pointer" onclick="loadPostContent(<?php echo($this->news_1['postid']); ?>)">
                     <strong><?php echo($this->news_1["title"]); ?></strong>
                 </h2>
-                <p style="position: absolute;bottom: 8px;right: 16px;"><i>Ngày tạo:<?php echo($this->news_1["datecreated"]); ?></i></p>
+                <p style="position: absolute;bottom: 8px;left: 16px;"><i>Ngày tạo:<?php echo($this->news_1["datecreated"]); ?></i></p>
             </div>
             <div class="col-6">
                 <?php
@@ -28,7 +28,7 @@
                 if(count($this->news_2) >0){
                     foreach($this->news_2 as &$news){
                         echo("
-                            <div class='row'>
+                            <div class='row ml-2' style='box-shadow: rgba(3, 102, 214, 0.3) 0px 0px 0px 3px;'>
                                 <h3 style='cursor:pointer' onclick='loadPostContent(".$news["postid"].")'>".$news["title"]."</h3>
                                 <span class='text-muted'><i>Ngày tạo: ".$news["datecreated"]."</i></span>
                             </div>
