44527252f1cd8639bbec6f6559cbd7de228c0404
nguyendao3105 <56407162+nguyendao3105@users.noreply.github.com>
2021-11-29 13:40:11 +0700

    add some ux

--- app/default/views/news/content.php
+++ app/default/views/news/content.php
@@ -1,15 +1,17 @@
 
 <section class="d-flex align-items-center" style="flex-direction:column; width: 90%;">
     <div class="col d-flex align-items-center" style="flex-direction:column; background-color:white;" >
-        <div class="row" >
-            <h1><?php echo($this->title) ?></h1>
-    </div>
-    <div class="row d-flex justify-content-center" >
-        <div class="col-8" >
-        <?php
-            include "public/posts/".$this->content.".html";
-        ?>
+        <div class="row d-flex justify-content-center" style="width:80%;" >
+            <div class="col-10">
+                <h1><?php echo($this->title) ?></h1>
+            </div>
         </div>
-    </div>
+        <div class="row d-flex justify-content-center" style="width:80%;">
+            <div class="col-10" >
+                <?php
+                    include "public/posts/".$this->content.".html";
+                ?>
+            </div>
+        </div>
     </div>
 </section>
--- app/default/views/news/index.php
+++ app/default/views/news/index.php
@@ -13,23 +13,22 @@
     include "app/default/views/component/header.php";
 ?>
 <section class="vh-100 d-flex justify-content-center" id="content">
-    <div style="width:70vw">
-        <div class="row mt-2">
-            <div class="col-6 px-0" style="background-color:white">
-                <img  style="max-height:60vh;width:100%;border-radius:0;box-shadow: rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;" src="public/images/news1_bg.jpg" alt="Description to news">
-                <h2 style="position: absolute;top: 8px;left: 16px;cursor:pointer" onclick="loadPostContent(<?php echo($this->news_1['postid']); ?>)">
+    <div style="width:70vw;height:100%;background-color:white;overflow:auto;display:block;">
+        <div class="row mt-1 ml-2" style="background-color:white;width:100%;">
+            <div class="col-6 px-0 ml-4 mr-2 mt-3" style="background-image: url('public/images/news1_bg.jpg');background-repeat: no-repeat;background-size: cover;">
+                <h2 style="cursor:pointer" onclick="loadPostContent(<?php echo($this->news_1['postid']); ?>)">
                     <strong><?php echo($this->news_1["title"]); ?></strong>
                 </h2>
-                <p style="position: absolute;bottom: 8px;left: 16px;"><i>Ngày tạo:<?php echo($this->news_1["datecreated"]); ?></i></p>
+                <p class="text-muted"><i>Ngày tạo:<?php echo($this->news_1["datecreated"]); ?></i></p>
             </div>
-            <div class="col-6">
+            <div class="col-5 px-0 ml-4">
                 <?php
                 if($this->news_2 != null){
                 if(count($this->news_2) >0){
                     foreach($this->news_2 as &$news){
                         echo("
-                            <div class='row ml-2 my-2' style='box-shadow: rgba(3, 102, 214, 0.3) 0px 0px 0px 3px;'>
-                                <h3 style='cursor:pointer' onclick='loadPostContent(".$news["postid"].")'>".$news["title"]."</h3>
+                            <div class='row my-2'>
+                                <h4 style='cursor:pointer' onclick='loadPostContent(".$news["postid"].")'>".$news["title"]."</h4>
                                 <span class='text-muted'><i>Ngày tạo: ".$news["datecreated"]."</i></span>
                             </div>
                         ");
@@ -44,10 +43,14 @@
             if(count($this->news_3) >0){
                 foreach($this->news_3 as &$news){
                     echo("
-                        <div class='row'>
-                        <h3 style='cursor:pointer' onclick='loadPostContent(".$news["postid"].")'>".$news["title"]."</h3>
-                   
+                    <div class='row pl-2' style='background-color:white;width:100%;'>
+                        <div class='col-6 my-2 ml-4'>
+                            <h4 style='cursor:pointer' onclick='loadPostContent(".$news["postid"].")'>".$news["title"]."</h4>
+                            <span class='text-muted'><i>Ngày tạo: ".$news["datecreated"]."</i></span>
                         </div>
+                        <div class='col-4'>
+                        </div>
+                    </div>
                     ");
                 }
             }
