7b0cb54ee949e31c4539f30b79c1a8e60378db47
Huu Hoang <nguyenhuuhoang171@gmail.com>
2021-11-29 19:56:24 +0700

    fix product responsive

--- app/default/views/product/product_page.php
+++ app/default/views/product/product_page.php
@@ -28,7 +28,7 @@
                     }
             ?>
                 <!-- frame for 1 product -->
-                <div class="col-md-3 mb-3">
+                <div class="mb-3 col-lg-3 col-md-4 col-sm-6 col-12">
                     <div class="product-top">
                         <img class="img-pro" width="100%" height="300px" src="./public/images/products/<?php echo $product['productid'];?>p0.png">
                         <div class="overlay">
