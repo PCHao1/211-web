e1f6d28b05c3b3f1e79d3c67c8531d3889d8aa0f
kimduc0103 <hoangkimanhduc01031999@gmail.com>
2021-11-29 07:59:29 +0700

    sua lai view cho showfb lan 4

--- app/default/views/feedback/showfb.php
+++ app/default/views/feedback/showfb.php
@@ -54,11 +54,11 @@ label.star:before {
 
    
 </head>
-<body>
-<section class="vh-100" style="background-color: #eee;">
-  <div class="container h-100">
+<body >
+<section style="background-color: #eee; height:50vh;">
+  <div class="container h-100" >
     <div class="row d-flex justify-content-center align-items-center h-100" style="overflow-y:auto;overflow-x:auto;">
-      <div class="col-lg-12 col-xl-11" >
+      <div class="col-lg-12 col-xl-11"  >
         <!-- <div class="card text-black" style="border-radius: 25px;">
           <div class="card-body p-md-5"> -->
             <!-- <div class="row justify-content-center">
